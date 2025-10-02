aaf3d57de5d6004447f2bb003cc0edbb827b1862
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-10-02 16:52:47 +0100

    adding passwords to test and dev user data

--- server/db/data/test-data/users.js
+++ server/db/data/test-data/users.js
@@ -2,26 +2,31 @@ module.exports = [
   {
     username: "admin",
     email: "admin@filmclub.com",
+    password: "admin123",
     role: "staff",
   },
   {
     username: "olive",
     email: "olive@example.com",
+    password: "olive123",
     role: "user",
   },
   {
     username: "toad",
     email: "toad@example.com",
+    password: "toad123",
     role: "user",
   },
   {
     username: "laura_palmer",
     email: "laura@example.com",
+    password: "laura123",
     role: "user",
   },
   {
     username: "dale_cooper",
     email: "cooper@example.com",
+    password: "cooper123",
     role: "user",
   },
 ];
