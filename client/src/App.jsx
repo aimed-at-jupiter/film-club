6b2b0c38cf8bfe0aad8bb42176df93d6ad5c8abc
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-10-09 17:36:35 +0100

    renaming files, adding blank pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
 function App() {
   return (
