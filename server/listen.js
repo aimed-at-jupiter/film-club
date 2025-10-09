3feb8588178c7c3c34c1f35e3350aecbefbd17b9
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-10-09 10:53:17 +0100

    prepping listen and scripts for production

--- server/listen.js
+++ server/listen.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 4100;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
