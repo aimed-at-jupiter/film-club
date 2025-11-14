33a0153e90a0f683fde4c01fa9ed9bf357065f12
aimed-at-jupiter <201750005+aimed-at-jupiter@users.noreply.github.com>
2025-11-14 16:03:17 +0000

    adding register import to authContext

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { postLogin } from "../api/postLogin";
+import { postRegister } from "../api/postRegister";
 import { useContext } from "react";
 
 export const AuthContext = createContext();
